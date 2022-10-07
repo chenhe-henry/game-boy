7eac5163268aefffdd8674d18bbd97aee371ed37
chenhe <henryhe.dev@gmail.com>
2022-10-08 08:21:20 +1100

    add new game street fighting

--- src/views/MonsterSlayer.vue
+++ src/views/MonsterSlayer.vue
@@ -1,19 +1,22 @@
 <template>
-  <div>
+  <div class="fighting__block">
     <div class="status">
       <section class="status__label">
         <h1>Player: {{ playerHealth }}</h1>
+        <h3 class="status__min-left" v-show="attackStart && playerMin !== 0">{{playerMin}}</h3>
       </section>
       <section class="status__label">
         <h1>VS</h1>
+        <h3 v-show="attackStart && (playerMin !== 0 || monsterMin !== 0)" class="hidden">VS</h3>
       </section>
       <section class="status__label">
         <h1>Monster: {{ monsterHealth }}</h1>
+        <h3 class="status__min-right" v-show="attackStart && monsterMin !== 0">{{monsterMin}}</h3>
       </section>
     </div>
-    <div>
-      <button @click="attack">Attack</button>
-      <button @click="autoAttack">Auto Attack</button>
+    <div class="btn-group">
+      <button @click="attack" :disabled="checkWin">Attack</button>
+      <!-- <button @click="autoAttack">Auto Attack</button> -->
       <button @click="restart">Restart</button>
     </div>
     <div v-if="checkWin">
@@ -28,8 +31,11 @@ export default {
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      playerMin: 0,
+      monsterMin: 0,
       gameIsOn: false,
       checkWin: false,
+      attackStart: false,
       winner: "",
     };
   },
@@ -40,8 +46,11 @@ export default {
       }, 1000);
     },
     attack() {
-      this.playerHealth -= this.calculateDamage(3, 10);
-      this.monsterHealth -= this.calculateDamage(5, 12);
+      this.attackStart = true
+      this.playerMin = -this.calculateDamage(3, 10);
+      this.monsterMin = -this.calculateDamage(5, 12);
+      this.playerHealth += this.playerMin;
+      this.monsterHealth += this.monsterMin;
       if (this.playerHealth <= 0) {
         this.checkWin = true;
         this.winner = "Monster";
@@ -49,6 +58,7 @@ export default {
         this.checkWin = true;
         this.winner = "Player";
       }
+      setTimeout(()=>{this.attackStart = false},2500)
     },
     calculateDamage(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -56,6 +66,8 @@ export default {
     restart() {
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.playerMin = 0;
+      this.monsterMin = 0;
       this.gameIsOn = false;
       this.checkWin = false;
       this.winner = "";
@@ -65,6 +77,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.fighting__block{
+  background: #fff;
+  width: 30vw;
+  height: 50vh;
+  margin: 0 auto;
+
+}
 .status {
   display: flex;
   justify-content: center;
@@ -72,6 +91,51 @@ export default {
   padding: 20px;
   &__label {
     margin: 0 20px;
+  }
+  &__min {
+    &-left{
+      color: red;
+      animation: dropMinLeft 3s 1 alternate;
+    }
+    &-right{
+      color: red;
+      animation: dropMinRight 3s 1 alternate;
+    }
+    
+  }
+}
+.hidden{
+  visibility: hidden;
+}
+.btn-group{
+  position: fixed;
+  bottom: 50%;
+  left: 50%;
+  transform: translate(-50%);
+}
+@keyframes dropMinRight {
+  // 0% {
+  //   opacity: 0;
+  // }
+  // 50% {
+  //   opacity: 1;
+  //   transform: translateY(2rem) translateX(2rem) rotate(45deg);
+  // }
+  to {
+    transform: translateY(4rem) translateX(4rem) rotate(90deg);
+  }
+}
+
+@keyframes dropMinLeft {
+  // 0% {
+  //   opacity: 0;
+  // }
+  // 50% {
+  //   opacity: 1;
+  //   transform: translateY(2rem) translateX(-2rem) rotate(-45deg);
+  // }
+  to {
+    transform: translateY(4rem) translateX(-4rem) rotate(-90deg);
   }
 }
 </style>
